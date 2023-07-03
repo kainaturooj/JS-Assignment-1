45ca8ca37a1f8c3d930e4942c24032b78089f9c8
kainaturooj <128933130+kainaturooj@users.noreply.github.com>
2023-07-03 16:50:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+////Q...Write a script to greet your website visitor using JS alert
+// box.
+
+
+///************coding************* */
+// alert("Welcom! to javascrit coding world");
+
+
+///Q..Write a script to display following message on your web
+// page:
+
+
+///************coding************* */
+// alert("Error! Please enter the valid password ");
+
+
+
+///Q...Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+
+///************coding************* */
+// alert("Welcom! to JS land \n Happy coding! ")
+
+
+///Q..4. Write a script to display following messages in sequence:
+
+
+///************coding************* */
+// alert("Welcom! to JS land");
+// alert("Happy coding! \n prevent this page from creating additional dialogs");
+
+
+///Q.Generate the following message through browser’s
+// developer console:
+
+
+///************coding************* */
+// console.log("Welcom! I can run JS through my web browser's console");
+
+
+
+
